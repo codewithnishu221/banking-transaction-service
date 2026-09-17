6ee13b71934ea197a23f16b7a9e52703d450cd2c
codewithnishu221 <nishumca22@gmail.com>
2026-09-17 13:10:20 +0530

    feat: add transaction service foundation

---
+++ src/main/java/com/banking/transaction_service/client/AccountServiceClient.java
@@ -0,0 +1,5 @@
+package com.banking.transaction_service.client;
+
+public class AccountServiceClient {
+
+}
---
+++ src/main/java/com/banking/transaction_service/controller/TransactionController.java
@@ -0,0 +1,5 @@
+package com.banking.transaction_service.controller;
+
+public class TransactionController {
+
+}
