951db9b338da1389e97825d08c8cf5d849f82c63
codewithnishu221 <nishumca22@gmail.com>
2026-09-17 17:31:12 +0530

    feat: implement transaction transfer flow

--- src/main/java/com/banking/transaction_service/client/AccountServiceClient.java
+++ src/main/java/com/banking/transaction_service/client/AccountServiceClient.java
@@ -1,5 +1,18 @@
 package com.banking.transaction_service.client;
 
-public class AccountServiceClient {
+import java.math.BigDecimal;
 
-}
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "account-service", url = "${account.service.url")
+public interface AccountServiceClient {
+
+
+    
+    @PutMapping("/api/v1/accounts/{accountNumber}/deduct")
+    String deductBalance(@PathVariable String accountNumber, @RequestParam  BigDecimal amount);
+    
+} 
--- src/main/java/com/banking/transaction_service/service/TransactionService.java
+++ src/main/java/com/banking/transaction_service/service/TransactionService.java
@@ -1,27 +1,111 @@
 package com.banking.transaction_service.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.banking.transaction_service.client.AccountServiceClient;
 import com.banking.transaction_service.dto.TransactionResponse;
 import com.banking.transaction_service.dto.TransferRequest;
+import com.banking.transaction_service.entity.Transaction;
+import com.banking.transaction_service.entity.TransactionStatus;
+import com.banking.transaction_service.entity.TransactionType;
+import com.banking.transaction_service.event.TransactionInitiatedEvent;
+import com.banking.transaction_service.repository.TransactionRepository;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+
+
+@Service 
+@Slf4j
+@RequiredArgsConstructor  
 public class TransactionService {
 
+    private final TransactionRepository transactionRepository;
+    private final AccountServiceClient accountServiceClient; 
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
+    private static  final String TRANSACTION_INITIATED_TOPIC = "transaction.initiated";
+    private static  final String TRANSACTION_COMPLETED_TOPIC = "transaction.completed";
+    private static  final String TRANSACTION_REFUNDED_TOPIC = "transaction.refunded";
+    
+    /*
+    * SAGA Step-1: Initiate transfer
+    * Deducts from sender via feign
+    * Saves transaction as Processing
+    * Publish event to kafka for fraud check
+    * Returns.
+    * @Param request
+    * @return  */
     public TransactionResponse transfer(TransferRequest request){
-        return  null;
+        log.info("SAGA START - Transfer: {} -> {} amount: {}", request.getSenderAccountNumber(), request.getReceiverAccountNumber(), request.getAmount());
+         // Saga step-1: Deduct from sender
+        accountServiceClient.deductBalance(request.getSenderAccountNumber(), request.getAmount());
+         Transaction transaction = new Transaction();
+         transaction.setAmount(request.getAmount());
+         transaction.setSenderAccountNumber(request.getSenderAccountNumber());
+         transaction.setReceiverAccountNumber(request.getSenderAccountNumber());
+         transaction.setType(TransactionType.TRANSFER);
+         transaction.setStatus(TransactionStatus.PROCESSING);
+         transaction.setDescription(request.getDescription());
+         transaction.setReferenceNumber(UUID.randomUUID().toString());
+         Transaction savedTransaction = transactionRepository.save(transaction);
+         log.info("Transaction saved as PROCESSING: {}", savedTransaction.getId());
+
+
+         // SAGA Step-2: Publish for fraud check
+         TransactionInitiatedEvent event = new TransactionInitiatedEvent(
+            savedTransaction.getId(),
+            savedTransaction.getSenderAccountNumber(),
+            savedTransaction.getReceiverAccountNumber(),
+            savedTransaction.getAmount(),
+            savedTransaction.getDescription()
+         );
+          
+         kafkaTemplate.send(TRANSACTION_INITIATED_TOPIC, savedTransaction.getId(), event);
+         log.info("SAGA Step 2 - Transaction InitiatedEvent published: {}", savedTransaction.getId());
+        return  mapToResponse(savedTransaction);
     }
+
+    private TransactionResponse mapToResponse(Transaction transaction){
+     TransactionResponse response = new TransactionResponse();
+        response.setId(transaction.getId());
+        response.setSenderAccountNumber(transaction.getSenderAccountNumber());
+        response.setReceiverAccountNumber(transaction.getReceiverAccountNumber());
+        response.setAmount(transaction.getAmount());
+        response.setType(transaction.getType());
+         response.setStatus(transaction.getStatus());
+        response.setDescription(transaction.getDescription());
+        response.setFailureReason(transaction.getFailureReason());
+        response.setReferenceNumber(transaction.getReferenceNumber());
+        response.setCreatedAt(transaction.getCreatedAt());
+        response.setCompletedAt(transaction.getCompletedAt());
+        return  response;
+    }
+
+
 
     public TransactionResponse getTransaction(String transactionId){
-        return  null;
+
+        return  mapToResponse(transactionRepository.findById(transactionId).orElseThrow(()-> 
+         new RuntimeException("Transaction not found: "+ transactionId)));
     }
 
-     public List getTransactionHistory(String accountNumber){
-        return  null;
+     public List<TransactionResponse> getTransactionHistory(String accountNumber){
+        return  transactionRepository.findBySenderAccountNumberOrderByCreatedAtDesc(accountNumber).stream()
+        .map(this::mapToResponse)
+        .collect(Collectors.toList());
     }
+
+    
     public TransactionResponse verifyOTP(String transactionId, String otp){
         return  null;
     }
